ba7222d6332123198e9474ce7ce7769702c7d5a5
Oluwatomisin Lalude <oluwatomisin1605@gmail.com>
2018-10-18 17:29:12 +0100

    Fix URL

--- js/main.js
+++ js/main.js
@@ -215,7 +215,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 } */
 if('serviceWorker' in navigator){
   window.addEventListener("load", () => {
-  navigator.serviceWorker.register('Restaurant-Review-App/sw.js')
+  navigator.serviceWorker.register('/Restaurant-Review-App/sw.js')
   .then((res) => {
     console.log(res);
   console.log( "Registration successful");
